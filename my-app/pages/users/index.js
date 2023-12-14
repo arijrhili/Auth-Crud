6fc3c361d4207abad42b8e0f1790b090ebfdf7af
arijrhili <arijrh427@gmail.com>
2023-12-14 20:23:02 +0100

    Update index.js

--- my-app/pages/users/index.js
+++ my-app/pages/users/index.js
@@ -10,13 +10,11 @@ export default function Home({ session }) {
   const [search, setSearch] = useState('');
   const [sortBy, setSortBy] = useState('');
   const [sortOrder, setSortOrder] = useState('asc');
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(9); // Adjust the page size as needed
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/users?search=${search}&sortBy=${sortBy}&sortOrder=${sortOrder}&page=${page}&pageSize=${pageSize}`);
+        const response = await fetch(`/api/users?search=${search}&sortBy=${sortBy}&sortOrder=${sortOrder}`);
         const data = await response.json();
         setUsers(data);
       } catch (error) {
@@ -25,17 +23,13 @@ export default function Home({ session }) {
     };
 
     fetchData();
-  }, [search, sortBy, sortOrder, page, pageSize]);
-
-  const handlePageChange = (newPage) => {
-    setPage(newPage);
-  };
+  }, [search, sortBy, sortOrder]);
 
   return (
     <div className="max-w-3xl mx-auto p-4 border border-gray-300 my-8 rounded-md shadow-md bg-white">
       <h1 className="text-4xl font-bold mb-8 text-center text-blue-600">Users Manager</h1>
       <div className="flex items-center justify-between mb-4">
-      <Link href="/users/adduser" className="text-blue-500 hover:underline">
+        <Link href="/users/adduser" className="text-blue-500 hover:underline">
           <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none">
             Add User
           </button>
@@ -82,16 +76,6 @@ export default function Home({ session }) {
           </li>
         ))}
       </ul>
-
-      <div className="flex justify-center mt-4">
-        <button onClick={() => handlePageChange(page - 1)} disabled={page <= 1} className="p-2 mx-1 bg-gray-200" >
-          Previous
-        </button>
-        <span className="mx-2">Page {page}</span>
-        <button onClick={() => handlePageChange(page + 1)} className="p-2 mx-1 bg-gray-200">
-          Next
-        </button>
-      </div>
     </div>
   );
 }
